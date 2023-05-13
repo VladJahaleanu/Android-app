0a2405e4a2e104a0f60c07c3d61c78cdfa671bf3
Alexandru-Daniel Tiganus <alexandu24@gmail.com>
2023-05-13 18:11:48 +0300

    initial layout

---
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val email: EditText = findViewById(R.id.email)
+        val password: EditText = findViewById(R.id.password)
+        val loginBtn: Button = findViewById(R.id.loginBtn)
+        val registerBtn: TextView = findViewById(R.id.registerBtn)
+
+        loginBtn.setOnClickListener {
+            @Override
+            fun onClick(view: View) {
+                val emailTxt: String = email.text.toString()
+                val passwordTxt: String = password.text.toString()
+
+                if(emailTxt.isEmpty() || passwordTxt.isEmpty()){
+                    Toast.makeText(applicationContext, "Complete all fields", Toast.LENGTH_SHORT).show()
+                }
+                else{
+
+                }
+            }
+        }
+    }
+}
